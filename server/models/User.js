ce0abfbd965d249b6e9f8eeb3ed1f9a844cbcbe3
Paul Asquith <p.n.asquith@gmail.com>
2021-05-30 14:17:39 +0100

    convert email to lowercase

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    email: { type: String, required: [true, 'Email is Required'] },
+    email: { type: String, required: [true, 'Email is Required'], lowercase: true },
     password: String,
 });
 
